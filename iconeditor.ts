b457d4fc578cad22df1f77efee69c5cced8642fd
Tom Ball <tballmsft@users.noreply.github.com>
2022-08-10 10:39:45 -0700

    start on icon editor

---
+++ iconeditor.ts
@@ -0,0 +1,13 @@
+namespace kojac {
+
+    class IconEditor extends Picker {
+        constructor(cursor: Cursor) {
+            super(cursor)
+            // make 5 x 5 matrix of buttons
+        }
+
+        public onButtonClicked(icon: string) {
+            // toggle the button state
+        }
+    }
+}
