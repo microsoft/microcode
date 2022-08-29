81e0ffdd7055443b72724057aa4290a984d9ecc3
Tom Ball <you@example.com>
2022-08-29 13:54:51 -0700

    move to game---light

--- main.ts
+++ main.ts
@@ -1,10 +1,4 @@
-// to compile with arcade.microsoft.com, the following two lines
-// should be uncommented
 type ImageG = Image
 const onEvent = control.onEvent
-
-// to compile with pxt-microbit (branch screen2), the above two
-// lines should commented out and the following line uncommented
-// const onEvent = control.onEvent2
 
 setTimeout(() => { const app = new kojac.App(); }, 1);
