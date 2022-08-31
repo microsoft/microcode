8d0e41f71ccc0a850615f0b359fcb1cd3251a78a
Tom Ball <you@example.com>
2022-08-31 12:55:50 -0700

    start on icon gallery

--- iconeditor.ts
+++ iconeditor.ts
@@ -1,16 +1,77 @@
 namespace microcode {
 
-    // TODO
-    // - set of buffers with preset icons
-    // - updated
-    
+
     const ledIcon1 = img`
-        1 . . . .
-        . 1 . . .
-        . . 1 . .
-        . . . 1 .
-        . . . . 1
+        . . . . .
+        . 1 . 1 .
+        . . . . .
+        1 . . . 1
+        . 1 1 1 .
     `
+    const ledIcon2 = img`
+        1 . . . 1
+        . 1 . 1 .
+        . . 1 . .
+        . 1 . 1 .
+        1 . . . 1
+    `
+    const ledIcon3 = img`
+        . . . . .
+        . 1 . 1 .
+        . . 1 . .
+        . 1 . 1 .
+        . . . . .
+    `
+    const ledIcon4 = img`
+        . . . . .
+        . 1 . 1 .
+        . . . . .
+        . 1 1 1 .
+        1 . . . 1
+    `
+    const ledIcons = [ledIcon1, ledIcon2, ledIcon3, ledIcon4]
+
+    // icon gallery
+
+    // - upscale 5x5 image to 16 x 16
+    function scaleUp(led55: ImageG) {
+        const ret = image.create(16, 16)
+        ret.fill(15)
+        for (let row = 0; row < 5; row++) {
+            for (let col = 0; col < 5; col++) {
+                const color = led55.getPixel(col, row) ? 2 : 12;
+                const nrow = 1 + row * 3, ncol = 1 + col * 3
+                ret.setPixel(ncol, nrow, color)
+                ret.setPixel(ncol + 1, nrow, color)
+                ret.setPixel(ncol, nrow + 1, color)
+                ret.setPixel(ncol + 1, nrow + 1, color)
+            }
+        }
+        return ret
+    }
+
+    // two modes for icon gallery
+    // 1. entrance to editor
+    // 2. picker
+    export class IconGallery extends Picker {
+        private scaledImages: Image[];
+        constructor(cursor: Cursor) {
+            super(cursor)
+            // TODO; make more memory efficient (reuse image if present)
+            this.scaledImages = ledIcons.map(scaleUp)
+            this.scaledImages.forEach((v,i)=> {
+                icons.reg["led55_"+i.toString()] = v
+            })
+            let btns: PickerButtonDef[] = this.scaledImages.map((v,i) => { return { icon: "led55_"+i.toString() } })
+            this.addGroup({label: "gallery", btns})
+        }
+
+        public onButtonClicked(button: PickerButton, icon: string) {
+            // go to Icon Editor
+        }
+    }
+
+    // icon editor
 
     function getPixel(col: number, row: number) {
         return ledIcon1.getPixel(col, row) ? tid.modifier.color_red : tid.modifier.color_darkpurple
