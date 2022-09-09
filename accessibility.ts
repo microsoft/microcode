78e87357fc14844bb8bd1247e25338fb244eb25f
pelikhan <jhalleux@microsoft.com>
2022-09-09 08:54:14 -0700

    fix shim

--- accessibility.ts
+++ accessibility.ts
@@ -2,6 +2,7 @@ namespace accessibility {
     /**
      * Notifies web application about the current content.
      */
+    //% shim=TD_ID
     export function setLiveContent(text: string) {
         control.simmessages.send("accessibility", Buffer.fromUTF8(text || ""))
     }
