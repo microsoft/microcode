1ce3b8b77b38ebdaedac1daff33f9b595c2fb5f5
Tom Ball <tballl@microsoft.com>
2022-09-15 15:48:50 -0700

    start on A/B for cursor navigation

---
+++ navigator.ts
@@ -0,0 +1,10 @@
+// encapsulate navigation so we can try A/B
+// experiment with cursor motion
+
+namespace microcode {
+    export class CursorNavigator {
+        private quadtree: QuadTree
+
+        constructor() {}
+    }
+}
