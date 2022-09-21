c0b85ee076423b4f82ee6d1b1b248054abc47a9c
Peli de Halleux <pelikhan@users.noreply.github.com>
2022-09-21 16:07:54 -0700

    Docs dump (#143)
    * dumping docs to files
    
    * export of icons
    
    * render whole program
    
    * hide cursor when rendering
    
    * comment out home
    
    * add name
    
    * updated logging
    
    * fix binary build
    
    * use image file names
    
    * wire menu
    
    * skip empty
    
    * fix name
    
    * better sample output

---
+++ docs.ts
@@ -0,0 +1,60 @@
+namespace microcode {
+    function imageToBuffer(img: Image) {
+        const w = img.width
+        const h = img.height
+        const buf = control.createBuffer(1 + w * h)
+        let j = 0
+        buf[j++] = w
+        for (let x = 0; x < w; ++x) {
+            for (let y = 0; y < h; ++y) {
+                buf[j++] = img.getPixel(x, y)
+            }
+        }
+        return buf
+    }
+
+    //% shim=TD_NOOP
+    export function init() {
+        control.simmessages.onReceived("docs:icons", () => dumpIcons())
+    }
+
+    //% shim=TD_NOOP
+    export function dumpIcons() {
+        renderIcons()
+    }
+
+    let editor: Editor
+    let editorName: string
+    export function dumpProgram(e: Editor, n: string) {
+        console.log(`dump program`)
+        editor = e
+        editorName = n
+        _dumpProgram()
+        editor = undefined
+        editorName = undefined
+    }
+
+    //% shim=TD_NOOP
+    function _dumpProgram() {
+        const prg = editor.renderProgram()
+        sendImage(editorName, prg)
+    }
+
+    function sendImage(name: string, img: Image) {
+        const msg = {
+            type: "image",
+            name: name.replace(" ", "_"),
+            pixels: imageToBuffer(img).toHex(),
+        }
+        control.simmessages.send("docs", Buffer.fromUTF8(JSON.stringify(msg)))
+    }
+
+    function renderIcons() {
+        icons.init()
+        for (const name of icons.names()) {
+            console.log(`dump ${name}`)
+            const icon = icons.get(name)
+            sendImage(`icon_${name}`, icon)
+        }
+    }
+}
