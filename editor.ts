59f15734377adac8dfd3c547ca9d9444f16628f3
Peli de Halleux <pelikhan@users.noreply.github.com>
2022-09-22 11:15:28 -0700

    move page icon to left, remove left right (#153)

--- editor.ts
+++ editor.ts
@@ -30,8 +30,6 @@ namespace microcode {
         private progdef: ProgramDefn
         private currPage: number
         private pageBtn: Button
-        private prevPageBtn: Button
-        private nextPageBtn: Button
         private pageEditor: PageEditor
         public cursor: Cursor
         private _changed: boolean
@@ -93,20 +91,6 @@ namespace microcode {
         public saveAndCompileProgram() {
             this.app.save(SAVESLOT_AUTO, this.progdef)
             new jacs.TopWriter().emitProgram(this.progdef)
-        }
-
-        private nextPage() {
-            let index = this.currPage + 1
-            index %= this.progdef.pages.length
-            this.switchToPage(index, false)
-        }
-
-        private prevPage() {
-            let index = this.currPage - 1
-            if (index < 0) {
-                index = this.progdef.pages.length - 1
-            }
-            this.switchToPage(index, false)
         }
 
         private pickPage() {
@@ -272,25 +256,9 @@ namespace microcode {
                 parent: this.hudroot,
                 style: ButtonStyles.BorderedPurple,
                 icon: PAGE_IDS[this.currPage],
-                x: 0,
+                x: Screen.LEFT_EDGE + 32,
                 y: 8,
                 onClick: () => this.pickPage(),
-            })
-            this.nextPageBtn = new EditorButton(this, {
-                parent: this.hudroot,
-                style: ButtonStyles.BorderedPurple,
-                icon: "next_page",
-                x: 19,
-                y: 8,
-                onClick: () => this.nextPage(),
-            })
-            this.prevPageBtn = new EditorButton(this, {
-                parent: this.hudroot,
-                style: ButtonStyles.BorderedPurple,
-                icon: "prev_page",
-                x: -19,
-                y: 8,
-                onClick: () => this.prevPage(),
             })
             this.progdef = this.app.load(SAVESLOT_AUTO)
         }
@@ -320,9 +288,7 @@ namespace microcode {
             } else this.navigator = new RuleRowNavigator()
 
             this.navigator.addButtons([
-                this.prevPageBtn,
                 this.pageBtn,
-                this.nextPageBtn,
             ])
 
             this.pageEditor.addToNavigator()
@@ -369,8 +335,6 @@ namespace microcode {
                 this.pageEditor.draw()
             }
             this.pageBtn.draw()
-            this.prevPageBtn.draw()
-            this.nextPageBtn.draw()
             this.picker.draw()
             this.cursor.draw()
         }
