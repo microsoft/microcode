12aaaeb89a689079af2e7d3bd51a0bae767b98a3
Tom Ball <tball@microsoft.com>
2022-09-30 21:15:10 -0700

    unary

--- options.ts
+++ options.ts
@@ -3,6 +3,6 @@ namespace microcode {
         public static toolTips: boolean = true
         public static repeatKey: boolean = false
         public static fps: boolean = false
-        public static coins: boolean = true
+        public static coins: boolean = false
     }
 }
