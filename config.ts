0743e870e85d055d060dde00ce1c30ec30d52771
pelikhan <jhalleux@microsoft.com>
2022-10-06 15:29:53 -0700

    fix home aniomation

--- config.ts
+++ config.ts
@@ -2,7 +2,6 @@ namespace userconfig {
     export const DISPLAY_CFG0 = 0x02000080 // don't wait for shield on startup
     // TODO this should be only enabled on micro:bit
 
-
     // doubled screen size on browser
     export const ARCADE_SCREEN_WIDTH = 240
     export const ARCADE_SCREEN_HEIGHT = 180
--- home.ts
+++ home.ts
@@ -72,7 +72,7 @@ namespace microcode {
             const t = control.millis()
             const dy = this.yOffset == 0 ? (Math.idiv(t, 800) & 1) - 1 : 0
             const margin = 2
-            const OFFSET = (Screen.HEIGHT >> 1) - microbitLogo.height - wordLogo.height - margin
+            const OFFSET = (Screen.HEIGHT >> 1) - wordLogo.height - margin
             const y = Screen.TOP_EDGE + OFFSET + dy
             Screen.drawTransparentImage(
                 wordLogo,
