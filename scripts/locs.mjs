245ca13c1e7949e3a46114d8322b99b827c377aa
pelikhan <jhalleux@microsoft.com>
2022-10-13 12:48:35 -0700

    generte all loc files

--- scripts/locs.mjs
+++ scripts/locs.mjs
@@ -1,10 +1,33 @@
-import { writeFileSync, readFileSync } from "fs"
-const tooltips = JSON.parse(
+import { writeFileSync, readFileSync, readdirSync } from "fs"
+import { join } from "path"
+
+const enTooltips = JSON.parse(
     readFileSync("./locales/tooltips.json", { encoding: "utf-8" })
 )
 
-const ts = `// auto-generated, run 'node scripts/locs.js' to refresh
+const writeLoc = lang => {
+    console.log(lang)
+    const fp = join(...[".", "locales", lang, "tooltips.json"].filter(s => !!s))
+    const op = `./tooltips${lang ? `.${lang}` : ""}.ts`
+    console.log(`convert ${fp} -> ${op}`)
+    const tooltips = JSON.parse(readFileSync(fp, { encoding: "utf-8" }))
+    // merge
+    Object.keys(enTooltips)
+        .filter(k => !tooltips[k])
+        .forEach(k => (tooltips[k] = enTooltips[k]))
+
+    const ts = `// auto-generated, run 'node scripts/locs.js' to refresh
 namespace microcode {
     export const tooltips: any = ${JSON.stringify(tooltips, null, 2)}
 }`
-writeFileSync("./tooltips.ts", ts)
+    writeFileSync(op, ts, { encoding: "utf8" })
+}
+
+const files = readdirSync("./locales", { withFileTypes: true }).filter(f =>
+    f.isDirectory()
+)
+
+writeLoc("")
+for (const file of files) {
+    const stat = writeLoc(file.name)
+}
