9031c4f676956beacd5df2fe42fbefe99ffddf12
Tom Ball <tball@microsoft.com>
2022-12-20 11:11:03 -0800

    add terminal to slider

--- tiles.ts
+++ tiles.ts
@@ -244,6 +244,7 @@ namespace microcode {
             TID_FILTER_COIN_4,
             TID_FILTER_COIN_5,
         ]
+        slider.constraints.handling = { terminal: true }
         slider.jdKind = JdKind.Slider
 
         const light = makeSensor(TID_SENSOR_LIGHT, "no_filters", 500)
