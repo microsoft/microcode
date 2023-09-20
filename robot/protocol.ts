13b415cf2b85e6698a25a7d9ca6e055a8319b7a9
Peli de Halleux <pelikhan@users.noreply.github.com>
2023-09-19 17:20:08 -0700

    Update robot/protocol.ts

--- robot/protocol.ts
+++ robot/protocol.ts
@@ -1,12 +1,26 @@
 namespace microcode {
     const MESSAGE_MAGIC = 0xf498
 
+    /**
+     * List of commands supported by the micro:bit robot program
+     */
     export enum RobotCommand {
+        /**
+         * Runs the robot forward and backward.
+         *     speed: i16 %
+         */
         MotorRun = 0x0001,
+        /**
+         * Turn the robot left and right. Right is positive values.
+         *     speed: i16 %
+         */
         MotorTurn = 0x0002,
         LedSetColor = 0x0010,
     }
 
+    /**
+     * Encodes the command and payload into a buffer that can be sent via radio
+     */
     export function createRobotCommand(cmd: RobotCommand, payload: Buffer) {
         const buf = pins.createBuffer(4 + payload.length)
         buf.setNumber(NumberFormat.UInt16LE, 0, MESSAGE_MAGIC)
@@ -16,6 +30,9 @@ namespace microcode {
         return buf
     }
 
+    /**
+     * Decodes message buffer
+     */
     export function readRobotCommand(msg: Buffer): { cmd: RobotCommand, payload: Buffer } {
         const magic = msg.getNumber(NumberFormat.UInt16LE, 0)
         if (magic !== MESSAGE_MAGIC) return undefined; // ignore
