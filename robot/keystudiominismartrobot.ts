26bb21de0995f296a92609e5432e5bee13e22b5a
Peli de Halleux <pelikhan@users.noreply.github.com>
2023-09-19 17:05:23 -0700

    blue = forward, red = backward

--- robot/keystudiominismartrobot.ts
+++ robot/keystudiominismartrobot.ts
@@ -2,10 +2,16 @@ namespace microcode {
     export class KeyStudioMiniSmartRobot implements Robot {
 
         motorRun(speed: number) {
-            if (speed === 0)
+            if (speed === 0) {
                 k_Bit.carStop()
+                this.ledSetColor(0, 0, 0)
+            }
             else {
                 const dir = speed >= 0 ? DIR.RunForward : DIR.RunBack
+                if (dir === DIR.RunBack)
+                    this.ledSetColor(0xff, 0, 0)
+                else
+                    this.ledSetColor(0, 0, 0xff)
                 k_Bit.run(dir, Math.abs(speed))
             }
         }
--- robot/test.ts
+++ robot/test.ts
@@ -1,7 +1,6 @@
 basic.forever(() => {
     [0, 100, -100, 50, -50].forEach(speed => {
         game.addScore(1)
-        microcode.robot.ledSetColor(speed > 0 ? 0xff : 0, 0, speed < 0 ? 0xff : 0)
         microcode.robot.motorRun(speed)
         basic.pause(1000)
         microcode.robot.motorRun(0)
