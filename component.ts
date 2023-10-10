924a291ad11d8d5de5adc7e420d8eec8f0718b9c
ramseyball <ramseyball@msn.com>
2023-10-09 20:35:31 -0600

    remove guts of Component

--- component.ts
+++ component.ts
@@ -1,52 +1,10 @@
 namespace microcode {
-    let id_sequence = 0
-
-    export type ComponentHandler = (comp: Component) => void
-
     export interface IKindable {
         kind: string
     }
 
     export abstract class Component implements IKindable {
-        private id_: number
-        private data_: any
-        private _destroyHandlers: ComponentHandler[]
-        private _destroyed = false
-
-        //% blockCombine block="id" callInDebugger
-        get id() {
-            return this.id_
-        }
-        get data(): any {
-            if (!this.data_) {
-                this.data_ = {}
-            }
-            return this.data_
-        }
-        get destroyed(): boolean {
-            return this._destroyed
-        }
-
-        constructor(public kind: string) {
-            this.id_ = id_sequence++
-        }
-
-        onDestroy(handler: ComponentHandler) {
-            this._destroyHandlers = this._destroyHandlers || []
-            this._destroyHandlers.push(handler)
-        }
-
-        /* virtual */ destroy() {
-            if (this._destroyed) console.warn(`double destroy on ${this.id}`)
-            this._destroyed = true
-            const handlers = this._destroyHandlers || []
-            this._destroyHandlers = undefined
-            for (const handler of handlers) {
-                handler(this)
-            }
-            this.data_ = undefined
-        }
-
+        /* abstract */ destroy() { }
         /* abstract */ update() {}
         /* abstract */ draw() {}
     }
