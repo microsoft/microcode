c561106506d9f6f745132e576f8dc83cc456da67
Tom Ball <tball@microsoft.com>
2023-10-15 09:51:34 -0700

    artwork bug fixed

--- docs.ts
+++ docs.ts
@@ -66,8 +66,9 @@ namespace docs {
             console.log(`render sample ${sample.label}`)
             const icon = microcode.icons.get(sample.icon, true)
             if (icon) appendImage(images, "icon_sample", sample.label, icon)
-            const src = sample.source
-            settings.writeString(microcode.SAVESLOT_AUTO, src)
+            const saved: microcode.SavedState = JSON.parse(sample.source)
+            const progdef = microcode.progDefnFromJson(saved.progdef)
+            app.save(microcode.SAVESLOT_AUTO, progdef)
             const res = _renderProgram()
             Object.keys(res).forEach(iname => {
                 appendImage(
