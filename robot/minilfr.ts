606387190e89440816cea0d775076b99a67804e2
riven <4771215@qq.com>
2023-10-16 11:52:21 +0800

    move minilfr back into robotdriver

--- robot/minilfr.ts
+++ robot/minilfr.ts
@@ -21,7 +21,6 @@ namespace microcode {
         private ultrasonicValue: number = 0
         private sensorValue: number[] = [0, 0, 0, 0, 0];
         private batteryValue: number = 0;
-        private lastReceivedMessageId: number = undefined
 
         constructor() { 
             super() 
@@ -50,6 +49,19 @@ namespace microcode {
                 } else if (tmp[0] === "M33") { 
                     this.mode = MiniLFRMode.IDLE
                 }
+            })
+
+            basic.forever(() => { 
+                // read line sensor
+                if (this.isSenorUpdated) {
+                    writeCmd('M10')
+                    this.isSenorUpdated = false
+                } else if (this.isUltrasonicUpdated) { 
+                    writeCmd('M7')
+                    this.isUltrasonicUpdated = false
+                }
+
+                basic.pause(50)
             })
 
             writeCmd("M23 14")
@@ -86,85 +98,6 @@ namespace microcode {
             return 0x1
         }
 
-        
-        dispatch(msg: robots.RobotMessage) {
-            if (!msg) return
-
-            const messageId = msg.messageId
-            if (this.lastReceivedMessageId === messageId) {
-                return // duplicate
-            }
-
-            // decode message
-            this.lastReceivedMessageId = messageId
-            const cmd = msg.cmd
-            const payload = msg.payload
-
-            led.plot(cmd, 0)
-
-        }
-
-        start() { 
-            radio.setGroup(1)
-            radio.onReceivedNumber(code => {
-                const msg = robots.decodeRobotCompactCommand(code)
-                if (this.lastReceivedMessageId === msg.messageId) {
-                    return // duplicate
-                }
-                this.lastReceivedMessageId = msg.messageId
-                const payload = msg.payload.getNumber(NumberFormat.Int16LE, 0)
-                console.log(`ra: ${msg.cmd} ${payload}`)
-
-                if (msg.cmd === robots.RobotCommand.MotorRun) {
-                    this.motorRun(payload, payload)
-                    this.playTone(440, 50)
-                } else if (msg.cmd === robots.RobotCommand.MotorTurn) {
-                    this.motorRun(payload, -payload)
-                    this.playTone(932, 50)
-                }
-            })
-            basic.forever(() => { 
-                // read line sensor
-                if (this.isSenorUpdated) {
-                    writeCmd('M10')
-                    this.isSenorUpdated = false
-                } else if (this.isUltrasonicUpdated) { 
-                    writeCmd('M7')
-                    this.isUltrasonicUpdated = false
-                }
-
-                basic.pause(50)
-            })
-
-            input.onButtonPressed(Button.A, () => {
-                // toggle radio group
-                robots.nextGroup()
-                basic.showNumber(microcode.robots.radioGroup, 100)
-                basic.clearScreen()
-            })
-
-            input.onButtonPressed(Button.B, () => { 
-                // linefollow mode
-                if (this.mode === MiniLFRMode.IDLE) {
-                    writeCmd('M31')
-                    this.mode = MiniLFRMode.LINE_FOLLOW
-                } else if (this.mode === MiniLFRMode.LINE_FOLLOW) {
-                    writeCmd('M32')
-                    this.mode = MiniLFRMode.OBSTACLE_AVOIDANCE
-                } else if (this.mode === MiniLFRMode.OBSTACLE_AVOIDANCE) { 
-                    writeCmd('M33')
-                    this.mode = MiniLFRMode.IDLE
-                }
-            })
-
-            input.onButtonPressed(Button.AB, function () {
-                // calibrate sensor
-                // 1. put the robot on a white surface with black line
-                // 2. press A+B, then the robot will rotate in place to calibrate the sensor
-                writeCmd('M310')
-            })
-
-        }
 
         updateSensorOnMatrix(): void {
             let img = images.createImage(`
@@ -196,6 +129,8 @@ namespace microcode {
      * Kittenbot MiniLFR
      */
     //% fixedInstances whenUsed block="Kittenbot minilfr"
-    export const kittenbotMiniLFR = new KittenbotMiniLFRRobot()
+    export const kittenbotMiniLFR = new RobotDriver(
+        new KittenbotMiniLFRRobot()
+    )
 
 }
