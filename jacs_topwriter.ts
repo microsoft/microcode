ac568d4d6573faf7b1fdeac22606666afc6eada5
Tom Ball <tball@microsoft.com>
2023-11-03 10:37:56 -0700

    reorder

--- jacs_topwriter.ts
+++ jacs_topwriter.ts
@@ -108,14 +108,6 @@ namespace jacs {
             if (!this.dispatcher) {
                 this.dispatcher = this.parent.addProc(this.name + "_disp")
                 this.parent.withProcedure(this.dispatcher, wr => {
-                    const enablers = needsEnable()
-                    if (enablers.indexOf(this.classIdentifier) >= 0) {
-                        this.parent.emitSetReg(this, JD_REG_INTENSITY, hex`01`)
-                        if (this.classIdentifier == serviceClass("radio")) {
-                            // set group to 1
-                            this.parent.emitSetReg(this, 0x80, hex`01`)
-                        }
-                    }                  
                     const wakers = needsWakeup()
                     const wakeup = wakers.find(
                         r => r.classId == this.classIdentifier
@@ -138,6 +130,15 @@ namespace jacs {
                             }
                         )
                     }
+                    const enablers = needsEnable()
+                    if (enablers.indexOf(this.classIdentifier) >= 0) {
+                        this.parent.emitSetReg(this, JD_REG_INTENSITY, hex`01`)
+                        if (this.classIdentifier == serviceClass("radio")) {
+                            // set group to 1
+                            this.parent.emitSetReg(this, 0x80, hex`01`)
+                        }
+                    }                  
+
                     this.top = wr.mkLabel("tp")
                     wr.emitLabel(this.top)
                     wr.emitStmt(Op.STMT1_WAIT_ROLE, [this.emit(wr)])
